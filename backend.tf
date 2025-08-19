89626abc2624f5fd9fdf78ddd11274811cd82fc9
Sibi04 <141929386+Sibi04@users.noreply.github.com>
2025-08-19 22:33:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "tf-state-proj-2-80" # S3 bucket name
     key            = "states/terraform.tfstate" # Path inside the bucket
-    region         =  "us-east-2" # AWS region for S3 bucket
+    region         =  "us-west-1" # AWS region for S3 bucket
   }
 }
